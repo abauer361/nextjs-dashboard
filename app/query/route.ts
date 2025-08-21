7cc067e04afdc0b37e9bee63910cea15eac53fd7
Andrew <abauer@chapman.edu>
2025-08-20 21:57:26 -0700

    Setting Up Your Database

--- app/query/route.ts
+++ app/query/route.ts
@@ -1,3 +1,5 @@
+// Commented out because it only needed to be run once
+
 // import postgres from 'postgres';
 
 // const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
@@ -13,14 +15,10 @@
 // 	return data;
 // }
 
-export async function GET() {
-  return Response.json({
-    message:
-      'Uncomment this file and remove this line. You can delete this file when you are finished.',
-  });
-  // try {
-  // 	return Response.json(await listInvoices());
-  // } catch (error) {
-  // 	return Response.json({ error }, { status: 500 });
-  // }
-}
+// export async function GET() {
+//   try {
+//   	return Response.json(await listInvoices());
+//   } catch (error) {
+//   	return Response.json({ error }, { status: 500 });
+//   }
+// }
